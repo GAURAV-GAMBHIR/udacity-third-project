e525f9439b1381822a582b7683051d4fb400728c
GAURAV-GAMBHIR <39501048+GAURAV-GAMBHIR@users.noreply.github.com>
2018-09-08 19:40:53 +0530

    Add files via upload

--- pythonlog.py
+++ pythonlog.py
@@ -1,50 +1,80 @@
 #!/usr/bin/env python3
+
 import psycopg2
-name = "news"
-# 1. What are the most popular three articles of all time?
-q1 = "select title,views from article_view limit 3"
-# 2. Who are the most popular article authors of all time?
-q2 = """select authors.name,sum(article_view.views) as views from
+import sys
+from datetime import date
+
+db_name ="news"
+
+query1 = "select title, count(title) from article_view WHERE GROUP BY title AND limit 3"
+
+query2 = """select authors.name,sum(article_view.views) as views from
 article_view,authors where authors.id = article_view.author
 group by authors.name order by views desc"""
-# 3. On which days did more than 1% of requests lead to errors?
-q3 = "select * from error_log_view where \"Percent Error\" > 1"
-# for store results
-q1_rslt = dict()
-q1_rslt['title'] = "a.The 3 most popular articles of all time are"
-q2_rslt = dict()
-q2_rslt['title'] = """b.The most popular article authors of
-all time are:\n"""
-q3_rslt = dict()
-q3_rslt['title'] = """c.Days with more than 1% of request that
-lead to an error"""
-# returns query result
+
+query3 = "select * from error_log_view where \"Percent Error\" > 1"
 
 
-def get_q_rslt(q):
-    db = psycopg2.connect(database=name)
-    c = db.cursor()
-    c.execute(q)
-    rslts = c.fetchall()
-    db.close()
-    return rslts
+def database_connect(db_query):
+
+    try:
+        conn = psycopg2.connect(database="news")
+        cursor = conn.cursor()
+        cursor.execute(sql_request)
+        results = cursor.fetchall()
+        conn.close()
+       
+    except psycopg2.Error as err:
+        print("Unable to connect to database. Exiting ...")
+        print(err)
+        sys.exit(1)
+    return results
+#cdff
+def get_popular_articles():
+    ""
+    articles = database_connect(query1)
+
+    # print header for popular articles
+    print('\nWhat are the most popular three articles of all time?\n')
+
+    # for every row print article name and nbr of views
+    for title, views in articles:
+        lst = "  " + '"' + title + '"' + " - " + str(views) + " views\n"
+        sys.stdout.write(lst)
 
 
-def print_q_rslts(q_rslt):
-    print(q_rslt['title'])
-    for rslt in q_rslt['rslts']:
-        print('\t' + str(rslt[0])+' -> '+str(rslt[1])+'views')
+# func to find the authors w/ most pg views sorted w/ most pop author at top
+def get_popular_authors():
+
+    authors = database_connect(query2)
+
+    # print header for popular authors
+    print('\nWho are the most popular article authors of all time?\n')
+
+    # for every row print author name and total views
+    for name, views in authors:
+        print("  ", name, "-", views, "views")
 
 
-def print_error_q_rslts(q_rslt):
-    print(q_rslt['title'])
-    for rslt in q_rslt['rslts']:
-        print('\t'+str(rslt[0])+' -> '+str(rslt[1])+'%')
-# stores query result
-q1_rslt['rslts'] = get_q_rslt(q1)
-q2_rslt['rslts'] = get_q_rslt(q2)
-q3_rslt['rslts'] = get_q_rslt(q3)
-# print formatted output
-print_q_rslts(q1_rslt)
-print_q_rslts(q2_rslt)
-print_error_q_rslts(q3_rslt)
+# function to find which days received more than 1% of error requests
+def get_day_errors():
+
+    err_days = database_connect(query3)
+
+    # print header for error days
+    print("\nOn which days did more than 1% of requests lead to errors?\n")
+
+    # for every row print date (Month DD, YYYY) and err %
+    for date, pct_errs in err_days:
+        bad_status = "  " + date + " - " + str(pct_errs) + "% errors\n"
+        sys.stdout.write(bad_status)
+        print('\n')
+
+
+# execute all three functions
+if __name__ == '__main__':
+    get_popular_articles()
+
+    get_popular_authors()
+
+    get_day_errors()
